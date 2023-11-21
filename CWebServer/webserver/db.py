c91628b23b5c8baa2a0255e82f2c7e489e93ef66
Victor Arvidsson <victarv@gmail.com>
2023-11-21 23:11:23 +0100

    Web server add communities and get posts

--- CWebServer/webserver/db.py
+++ CWebServer/webserver/db.py
@@ -1,9 +1,33 @@
 from flask import current_app
 import requests
+import logging
 
 
 def get_communities():
     resp = requests.get(
         f'http://{current_app.config["DATABASE_HANDLER_URI"]}/communities'
     )
+    return resp.json()
+
+
+def post_communities(communities):
+    resp = requests.post(
+        f'http://{current_app.config["DATABASE_HANDLER_URI"]}/communities',
+        json=communities,
+    )
+    if resp.status_code != 200:
+        logging.debug(resp.reason)
+        logging.debug(resp.request.method, resp.request.headers, resp.request.body)
+        logging.debug(resp.text)
+    return resp.status_code
+
+
+def get_posts(communityId):
+    resp = requests.get(
+        f'http://{current_app.config["DATABASE_HANDLER_URI"]}/posts',
+        params={"communityId": communityId},
+    )
+    print(resp.request.method)
+    print(resp.request.url)
+    print(resp.request.body, flush=True)  
     return resp.json()
--- CWebServer/webserver/web.py
+++ CWebServer/webserver/web.py
@@ -1,9 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, abort, redirect, url_for
 from . import db
 
-bp = Blueprint("Web", __name__, url_prefix="/web")
+bp = Blueprint("web", __name__, url_prefix="/web")
 
 
 @bp.route("/")
 def index():
     return db.get_communities()
+
+
+@bp.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        instance_urL = request.form["instance_url"]
+        community_name = request.form["community_name"]
+        result = db.post_communities(
+            {"instanceUrl": instance_urL, "name": community_name}
+        )
+        if result != 200:
+            abort(400, "Something went wrong, please try again.")
+        redirect(url_for("web.index"))
+    return render_template("add.html")
+
+
+@bp.route("/<int:communityId>/posts")
+def post(communityId: int):
+    return db.get_posts(communityId)
