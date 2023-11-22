2728bccba2e9f2b4c8235ec50d38118634b9f458
Victor Arvidsson <victarv@gmail.com>
2023-11-22 23:35:34 +0100

    CWebserver started work on frontend

--- CWebServer/webserver/web.py
+++ CWebServer/webserver/web.py
@@ -1,12 +1,19 @@
 from flask import Blueprint, request, render_template, abort, redirect, url_for
 from . import db
+from markdown import markdown
+from bs4 import BeautifulSoup
 
 bp = Blueprint("web", __name__, url_prefix="/web")
 
 
 @bp.route("/")
 def index():
-    return db.get_communities()
+    communities = db.get_communities()
+    for community in communities:
+        if community["description"]:
+            html = markdown(community["description"])
+            community["description"] = BeautifulSoup(html, features='html.parser').get_text()
+    return render_template('index.html', communities=communities)
 
 
 @bp.route("/add", methods=["GET", "POST"])
@@ -19,7 +26,7 @@ def add():
         )
         if result != 200:
             abort(400, "Something went wrong, please try again.")
-        redirect(url_for("web.index"))
+        return redirect(url_for("web.index"))
     return render_template("add.html")
 
 
