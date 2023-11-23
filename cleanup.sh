d06750533755e4147be115b806d3d90c2bd5f362
Victor Arvidsson <victarv@gmail.com>
2023-11-24 00:22:59 +0100

    Moved all deployment into one file and fixed deployment script

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 kubectl delete --cascade='foreground' -f assignment1.yml
-kubectl delete --cascade='foreground' -f assignment1-rabbitmq.yml
 kubectl delete --cascade='foreground' -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
+kubectl delete pvc --all
