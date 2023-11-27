fe1c7c4afe81898a2d518445807e268f58e05290
Victor Arvidsson <victarv@gmail.com>
2023-11-28 00:15:25 +0100

    Cleanup disconnect from queue before deleting

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
+kubectl delete --cascade='foreground' -f assignment1.yml -l app=cworker --grace-period=30
+kubectl delete --cascade='foreground' -f assignment1.yml -l app=cfetcher--grace-period=30
 kubectl delete --cascade='foreground' -f assignment1.yml --grace-period=30
 kubectl delete --cascade='foreground' -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml" --grace-period=30
 kubectl delete pvc --all
