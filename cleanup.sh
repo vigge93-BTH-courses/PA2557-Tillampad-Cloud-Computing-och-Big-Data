80c90a94da10f0109a9fdc952bb6a069f7826088
Victor Arvidsson <victarv@gmail.com>
2023-11-28 12:48:06 +0100

    Dynamic sharding configuration

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,11 @@
 kubectl delete --cascade='foreground' -f assignment1.yml -l app=cworker --grace-period=30
 kubectl delete --cascade='foreground' -f assignment1.yml -l app=cfetcher--grace-period=30
 kubectl delete --cascade='foreground' -f assignment1.yml --grace-period=30
+for file in tmp
+do
+  kubectl delete --cascade='foreground' -f $file
+done
 kubectl delete --cascade='foreground' -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml" --grace-period=30
 kubectl delete pvc --all
+rm tmp/shard-*.yml
+rmdir tmp
--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,33 @@
-# Start queue related services
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+NUM_SHARDS=2
+
+echo -e "${CYAN}Starting RabbitMQ...${NC}"
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
 kubectl apply -f assignment1.yml -l app=c-message-q
 
-# Generate secret key for cwebserver
+echo -e "${CYAN}Generating webserver secret key...${NC}"
 kubectl delete secret cwebserver-secret-key --ignore-not-found
-kubectl create secret generic cwebserver-secret-key --from-literal=secret-key='123'
+key=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 24)
+kubectl create secret generic cwebserver-secret-key --from-literal=secret-key="${key}"
 
-# Start database services
+echo -e "${CYAN}Creating shard deploy templates...${NC}"
+mkdir tmp
+for ((shrd=1; shrd<=$NUM_SHARDS; shrd++)) do
+    sed "s/<num>/$shrd/g" shard-template.yml > tmp/shard-$shrd.yml
+    kubectl apply -f tmp/shard-$shrd.yml
+done
+
+
+echo -e "${CYAN}Starting communitydb and postdb-manager...${NC}"
 kubectl apply -f assignment1.yml -l app=communitydb
 kubectl apply -f assignment1.yml -l app=postdb-manager
-kubectl apply -f assignment1.yml -l app=postdb-shrd-1
-kubectl apply -f assignment1.yml -l app=postdb-shrd-2
 
-# Configure CommunityDB
+echo -e "${CYAN}Waiting for communitydb rollout to finish...${NC}"
 kubectl rollout status statefulset communitydb
 
+echo -e "${CYAN}Initiating communitydb replicaset...${NC}"
 community_initcommand='rs.initiate({_id: "CommunityRepSet", members: [
      { _id: 0, host: "communitydb-0.communitydb-service:27017" },
      { _id: 1, host: "communitydb-1.communitydb-service:27017" },
@@ -22,43 +35,50 @@ community_initcommand='rs.initiate({_id: "CommunityRepSet", members: [
 ]});'
 kubectl exec -i communitydb-0 -- mongosh --quiet --eval "$community_initcommand"
 
-initwait="while('PRIMARY' != rs.status()['members'][0]['stateStr']) { sleep(1000); console.log('Waiting to become PRIMARY...'); }"
 
-# Configure PostDB sharded cluster
+for ((shrd=1; shrd<=$NUM_SHARDS; shrd++)) do
+    echo -e "${CYAN}Waiting for shard $shrd rollout to finish...${NC}"
+    kubectl rollout status statefulset postdb-shrd-$shrd
+    echo -e "${CYAN}Initiating shard $shrd replicaset...${NC}"
+    shard_initcommand="rs.initiate({_id: \"postShrd${shrd}RepSet\", members: [
+        { _id: 0, host: \"postdb-shrd-${shrd}-0.postdb-shrd-${shrd}-service:27018\" },
+        { _id: 1, host: \"postdb-shrd-${shrd}-1.postdb-shrd-${shrd}-service:27018\" },
+        { _id: 2, host: \"postdb-shrd-${shrd}-2.postdb-shrd-${shrd}-service:27018\" },
+    ]});"
+    kubectl exec -i postdb-shrd-$shrd-0 -- mongosh --port 27018 --quiet --eval "$shard_initcommand"
+done
+
+echo -e "${CYAN}Waiting for postdb-manager rollout to finish...${NC}"
 kubectl rollout status statefulset postdb-manager
-kubectl rollout status statefulset postdb-shrd-1
-kubectl rollout status statefulset postdb-shrd-2
 
+echo -e "${CYAN}Initiating communitydb replicaset...${NC}"
 postMan_initcommand='rs.initiate({_id: "postManRepSet", configsvr: true, members: [
      { _id: 0, host: "postdb-manager-0.postdb-manager-service:27019" },
      { _id: 1, host: "postdb-manager-1.postdb-manager-service:27019" },
      { _id: 2, host: "postdb-manager-2.postdb-manager-service:27019" },
 ]});'
-shard1_initcommand='rs.initiate({_id: "postShrd1RepSet", members: [
-     { _id: 0, host: "postdb-shrd-1-0.postdb-shrd-1-service:27018" },
-     { _id: 1, host: "postdb-shrd-1-1.postdb-shrd-1-service:27018" },
-     { _id: 2, host: "postdb-shrd-1-2.postdb-shrd-1-service:27018" },
-]});'
-shard2_initcommand='rs.initiate({_id: "postShrd2RepSet", version: 1, members: [
-     { _id: 0, host: "postdb-shrd-2-0.postdb-shrd-2-service:27018" },
-     { _id: 1, host: "postdb-shrd-2-1.postdb-shrd-2-service:27018" },
-     { _id: 2, host: "postdb-shrd-2-2.postdb-shrd-2-service:27018" },
-]});'
-
 kubectl exec -i postdb-manager-0 -- mongosh --port 27019 --quiet --eval "$postMan_initcommand"
-kubectl exec -i postdb-shrd-1-0 -- mongosh --port 27018 --quiet --eval "$shard1_initcommand"
-kubectl exec -i postdb-shrd-2-0 -- mongosh --port 27018 --quiet --eval "$shard2_initcommand"
 
-# Create and configure mongos for Sharded cluster
+echo -e "${CYAN}Starting mongos for sharding and waiting for rollout to finish...${NC}"
 kubectl apply -f assignment1.yml -l app=postdb-mongos
 kubectl rollout status statefulset postdb-mongos
-kubectl exec -i postdb-mongos-0 -- mongosh --quiet --eval 'sh.addShard("postShrd1RepSet/postdb-shrd-1-0.postdb-shrd-1-service:27018,postdb-shrd-1-1.postdb-shrd-1-service:27018,postdb-shrd-1-2.postdb-shrd-1-service:27018")'
-kubectl exec -i postdb-mongos-0 -- mongosh --quiet --eval 'sh.addShard("postShrd2RepSet/postdb-shrd-2-0.postdb-shrd-2-service:27018,postdb-shrd-2-1.postdb-shrd-2-service:27018,postdb-shrd-2-2.postdb-shrd-2-service:27018")'
+
+echo -e "${CYAN}Adding shards to mongos...${NC}"
+for ((shrd=1; shrd<=$NUM_SHARDS; shrd++)) do
+    kubectl exec -i postdb-mongos-0 -- mongosh --quiet --eval "sh.addShard(
+        \"postShrd${shrd}RepSet/postdb-shrd-${shrd}-0.postdb-shrd-${shrd}-service:27018,postdb-shrd-${shrd}-1.postdb-shrd-${shrd}-service:27018,postdb-shrd-${shrd}-2.postdb-shrd-${shrd}-service:27018\")"
+done
+
+echo -e "${CYAN}Sharding posts collection...${NC}"
 kubectl exec -i postdb-mongos-0 -- mongosh --quiet --eval 'sh.shardCollection("PostDB.posts", {community_id: "hashed"})'
 
-# Make sure that the queue service has started before deploying the rest.
+echo -e "${CYAN}Waiting for RabbitMQ to finish rollout...${NC}"
 kubectl rollout status statefulset c-message-q-server
 
-# Deploy everyting else and wait for webserver to start
+echo -e "${CYAN}Deploying other services...${NC}"
 kubectl apply -f assignment1.yml
+
+echo -e "${CYAN}Waiting for web server rollout to finish...${NC}"
 kubectl rollout status deployment cwebserver
+
+echo -e "${CYAN}Deployment done!${NC}"
